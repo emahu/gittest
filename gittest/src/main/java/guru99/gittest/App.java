d6711df8418d54a0bdcb675e8fb089ea0dfc2139
intelrain <intelrain@163.com>
2018-08-08 10:08:29 +0800

    Commit

---
+++ gittest/src/main/java/guru99/gittest/App.java
@@ -0,0 +1,15 @@
+package guru99.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! HelloWorld" );
+        
+        System.out.println("Hello ");
+    }
+}
